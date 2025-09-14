bbfb8606bb3f5c484578d5873800e6892d5d37fe
James Arthur Appiah <james.galahad@gmail.com>
2025-09-14 20:52:19 +0000

    Add script that prints languages using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints 3 lines using an array and a loop
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
