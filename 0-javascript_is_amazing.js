3fbc3a1a9e8c1f898acd41512fcf5476b0eea1aa
James Arthur Appiah <james.galahad@gmail.com>
2025-09-14 20:28:11 +0000

    Add script that prints 'JavaScript is amazing'

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,1 +1,5 @@
-﻿const myVar = 'JavaScript is amazing'; console.log(myVar);
+﻿#!/usr/bin/node
+// A script that prints “JavaScript is amazing”
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
